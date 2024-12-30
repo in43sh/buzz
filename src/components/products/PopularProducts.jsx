abba39e8ffe49515b7afaf6f18879c5be9d52f2b
Sergey <in43sh@gmail.com>
2024-12-30 03:01:30 -0800

    Change localhost to env variable

--- src/components/products/PopularProducts.jsx
+++ src/components/products/PopularProducts.jsx
@@ -1,26 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 // import { Link } from 'react-router-dom';
 import Product from './Product';
 
 function PopularProducts(props) {
   const [products, setProducts] = useState([]);
+  const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+  // console.log('BASE_URL ===> ', BASE_URL);
 
-  const fetchProducts = async () => {
+  const fetchProducts = useCallback(async () => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
-    fetch('http://localhost:8080/products', requestOptions)
+    fetch(`${BASE_URL}/products`, requestOptions)
       .then((response) => response.json())
       .then((data) => {
         setProducts(data.users);
       })
       .catch((error) => console.error('Fetch Error:', error));
-  };
+  }, [BASE_URL]);
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [fetchProducts]);
 
   return (
     <section className={'product-cards-container ' + props.className}>
